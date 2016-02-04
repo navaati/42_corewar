0f44e68c5f8d123d32c0e1c70450a9c209437d26
Léo Gillot-Lamure <leo.gillot@navaati.net>
2016-02-04 16:25:08 +0100

    Reorganize champion loading code

--- lib/corewar.c
+++ lib/corewar.c
@@ -160,62 +160,6 @@ bool	cycle(t_vm *vm)
 	return (!LIST_EMPTY(&vm->procs));
 }
 
-static void	copy_to_memory(t_vm *vm, t_address pc, void *buf, t_offset size)
-{
-	t_offset	room_until_end;
-
-	room_until_end = MEM_SIZE - pc;
-	if (size < room_until_end)
-		ft_memcpy(vm->memory + pc, buf, size);
-	else
-	{
-		ft_memcpy(vm->memory + pc, buf, room_until_end);
-		ft_memcpy(vm->memory, buf + room_until_end, size - room_until_end);
-	}
-}
-
-static t_err	load_champion(t_vm *vm, t_address pc, header_t *header, void *buf)
-{
-	t_champion	*champ;
-	t_proc		*proc;
-
-	assert(vm->nb_champions <= MAX_PLAYERS);
-	if (vm->nb_champions >= MAX_PLAYERS)
-		return (ERR);
-	champ = &vm->champions[vm->nb_champions];
-	ft_memcpy(champ->name, header->prog_name, PROG_NAME_LENGTH);
-	champ->name[PROG_NAME_LENGTH] = '\0';
-	ft_memcpy(champ->comment, header->comment, COMMENT_LENGTH);
-	champ->comment[COMMENT_LENGTH] = '\0';
-	copy_to_memory(vm, pc, buf, header->prog_size);
-	proc = allocate_proc_node(vm);
-	*proc = (t_proc){
-		.vm = vm,
-		.pc = pc,
-		.carry = false,
-		.regs = { vm->nb_champions, 0 },
-		.live = false
-	};
-	proc->wait = get_curr_op(proc).delay;
-	proc->carry = true; // temporary to allow jumping
-	vm->nb_champions++;
-	return (OK);
-}
-
-static t_err	load_champion_from_file(t_vm *vm, t_address pc, void *file, size_t size)
-{
-	header_t	*header;
-	void		*buf;
-
-	if (size <= sizeof(header_t))
-		return (ERR);
-	header = (header_t *)file;
-	if (size - sizeof(header_t) < header->prog_size)
-		return (ERR);
-	buf = file + sizeof(header_t);
-	return (load_champion(vm, pc, header, buf));
-}
-
 t_err	init_vm(t_vm *vm,
 				void *files[], size_t sizes[], size_t nb_champs)
 {
--- lib/corewar_priv.h
+++ lib/corewar_priv.h
@@ -14,11 +14,22 @@ typedef struct	s_op
 	t_op_exec	exec;
 }				t_op;
 
+typedef struct	s_champ_desc
+{
+	char		*name;
+	char		*comment;
+	uint16_t	size;
+	void		*buf;
+}				t_champ_desc;
+
 uint8_t		deref(t_proc *proc, t_address addr);
 t_word		deref_word(t_proc *proc, t_address addr);
 t_address	deref_short(t_proc *proc, t_address addr);
 t_word		deref_ind(t_proc *proc, t_address addr);
 t_proc		*allocate_proc_node(t_vm *vm);
+t_op		get_curr_op(t_proc *proc);
+t_err		load_champion_from_file(t_vm *vm, t_address pc,
+									void *file, size_t size);
 
 extern const t_op	op_tab[];
 
