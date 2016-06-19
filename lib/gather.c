9642674f73d41351bdbf55c112856b62dc1225b5
Zleub <debray.arnaud@gmail.com>
2016-06-19 15:25:40 +0200

    extern generic gatherer

---
+++ lib/gather.c
@@ -0,0 +1,40 @@
+#include "corewar_priv.h"
+
+uint8_t gather_dir(t_proc *proc, const t_op *op, t_args *args, uint8_t i)
+{
+	args->len += 4;
+	proc->pc += 4;
+	if (op->params[i] & T_DIR) {
+		args->fields[i].param = deref_word(proc, -4);
+		args->nbr += 1;
+	}
+	else
+		return (0);
+	return (1);
+}
+
+uint8_t gather_ind(t_proc *proc, const t_op *op, t_args *args, uint8_t i)
+{
+	args->len += 2;
+	proc->pc += 2;
+	if (op->params[i] & T_IND) {
+		args->fields[i].param = deref_short(proc, -2);
+		args->nbr += 1;
+	}
+	else
+		return (0);
+	return (1);
+}
+
+uint8_t gather_reg(t_proc *proc, const t_op *op, t_args *args, uint8_t i)
+{
+	args->len += 1;
+	proc->pc += 1;
+	if (op->params[i] & T_REG) {
+		args->fields[i].param = deref(proc, -1);
+		args->nbr += 1;
+	}
+	else
+		return (0);
+	return (1);
+}
