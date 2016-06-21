8536c215b11a2be2e8cd6999690912f357560e49
Zleub <debray.arnaud@gmail.com>
2016-06-21 15:28:30 +0200

    more tests

--- tests/hexgenerator.lua
+++ tests/hexgenerator.lua
@@ -1,3 +1,6 @@
+local time = os.time()
+math.randomseed(time)
+
 local f = io.open('test.hex', 'wb')
 
 function tohex(str)
@@ -18,12 +21,13 @@ end
 local header_description = "4661726e6f61726e6f61726e6f"
 
 tohex(header_description)
+
 for i=1,2040 do
 	f:write(string.char(0))
 end
 
 for i=1,70 do
-	f:write(string.char(math.random(1, 10)))
+	f:write(string.char(math.random(1, 16)))
 end
 
 f:close()
